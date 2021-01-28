c18925cc0d71470833f11a7b6e6c20bf9f206137
Alex Spies <alex@afspies.com>
2021-01-28 13:19:53 +0000

    Update plot function to have improved darkmode colors for characters. Not takes pytorch dataloader and tsne X,Y directly

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,16 +1,20 @@
 import altair as alt
 alt.data_transformers.disable_max_rows()
-from vega_datasets import data
 import pandas as pd
+import torch
 
-#TODO Change input format to default scikit T-SNE Format
-def plot_tsne(in_data, num_points=1000, darkmode=True):
+def plot_tsne(tsne_xy, dataloader, num_points=1000, darkmode=True):
     # import IPython # Try to automatically detect darkmode - colab is blocking my DOM request
     # # html[theme=dark]
     # js_code = r'document.documentElement.getAttribute("theme");'
     # display(IPython.display.Javascript(js_code))
-    num_points = min(num_points, len(in_data))
-    data = in_data.sample(n=num_points, replace=False)
+
+    images, labels = zip(*[(x[0].numpy()[0,:,:,None], x[1]) for x in dataloader.dataset])
+
+    num_points = min(num_points, len(labels))
+    data = pd.DataFrame({'x':tsne_xy[:, 0], 'y':tsne_xy[:, 1], 'label':labels,
+                        'image': images})
+    data = data.sample(n=num_points, replace=False)
 
     alt.renderers.set_embed_options(theme='dark' if darkmode else 'light')
     selection = alt.selection_single(on='mouseover', clear='false', nearest=True,
@@ -48,8 +52,11 @@ def plot_tsne(in_data, num_points=1000, darkmode=True):
     ).mark_rect(stroke='black',strokeWidth=0).encode(
         alt.X('column:O', axis=None),
         alt.Y('row:O', axis=None),
-        alt.Color('image:Q',
-            scale=alt.Scale(scheme=alt.SchemeParams('greys', extent=[1, 0])),
+        alt.Color('image:Q',sort='descending',
+            scale=alt.Scale(scheme=alt.SchemeParams('darkblue' if darkmode else 'lightgreyteal',
+                            extent=[1, 0]),
+            
+            ),
             legend=None
         ),
     ).properties(
@@ -58,4 +65,3 @@ def plot_tsne(in_data, num_points=1000, darkmode=True):
     )
 
     return scatter | digit
-
